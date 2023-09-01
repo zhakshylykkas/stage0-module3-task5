9fb2f2fc14adef3f41ae9c14670ee95e4ae7c71f
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-01 18:31:19 +0600

    ThreeStuckVars task commit after the second testing

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -3,6 +3,6 @@ package lang.print.gaps.task5;
 public class ThreeStuckVars {
     public static void main(String[] args) {
         int first = 1, second = 10, third = 100;
-        System.out.println(first*100 + second + third/100);
+        System.out.println(first*100000 + second*1000 + third);
     }
 }
